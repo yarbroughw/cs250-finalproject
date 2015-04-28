317ea5d4e5c865f7e4435aa473fad3a5a3f496fd
Alden S. Page <pagea@allegheny.edu>
2015-04-27 22:01:09 -0400

    Added wrapper for circle call; made code for drawing colliding circles differently

--- src/front.py
+++ src/front.py
@@ -1,15 +1,29 @@
 import pygame
 import sys
 import model
+import controller
 
+# Color of non-colliding circles
+blue = (138, 255, 255)
+# Color of colliding circles
+orange = (255, 170, 51)
+
+
+def draw_circ(circle, color)
+    '''A nice wrapper for pygame's ugly-ass draw syntax. '''
+    pygame.gfxdraw.filled_circle(screen, c.coord[0], c.coord[1], c.radius, color)
 
 def update_frame(screen):
     # Draw the background
     screen.fill((255, 255, 255))
     
     # Draw all of the circles
-    for circle in controller.world:
-
+    for c in controller.world:
+        #Are we colliding? If so, change the color
+        if c.collide is False:
+            draw_circ(c, blue)
+        else:
+            draw_circ(c, orange)
 
     # Update the screen
     pygame.display.update()
