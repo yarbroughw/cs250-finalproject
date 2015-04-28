d1000cf571f5a0f00bb02b5a776efe8fc6575855
Alden S. Page <pagea@allegheny.edu>
2015-04-27 23:31:55 -0400

    Added some argument detection

--- src/controller.py
+++ src/controller.py
@@ -1,20 +1,31 @@
+import sys
 import pygame
 import front
 import model
+import collision
 
 WIN_SIZE = (1024, 768)
 
-def run_sim():
+def run_sim(arg):
+    detector = None
+    if arg is 0:
+        print("No collision detection enabled.")
+    if arg is 1:
+        print("Using brute force collision detection.")
+        detector = collision.BasicDetector()
+    if arg is 2:
+        print("Using Quadtree collision detection.")
+        detector = collision.QuadtreeDetector()
+
     pygame.init()
-    clock = pygame.time.Clock()
+    # clock = pygame.time.Clock()
+    world = model.World(WIN_SIZE[0], WIN_SIZE[1], 50)
     screen = pygame.display.set_mode(WIN_SIZE)
-    world = model.World(WIN_SIZE[0], WIN_SIZE[1], 10)
-    
     # Main loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            front.update_frame(screen)
-            world.step(None)
+        front.update_frame(world, screen)
+        world.step(detector)
